0e75c20793e2a370aa414ef53ffd317bbf40243f
JSaturn9 <JSaturn9@github.com>
2021-04-30 19:50:21 -0400

    Created AboutMe Component

---
+++ src/components/AboutMe.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+const AboutMe = () => (
+    <Container>
+        <div>
+            <h2>This will be about me component</h2>
+            <p>Hi! I am a front end web developer with a particular interest and passion for React and its capability to build powerful UI. 
+               I have a Bachelor's Degree in Art and Design with an emphasis in Graphic Design. 
+               When I realized that graphic design wasn't for me I took a detour to do social work and marketing but started to miss the creativity and design I was able to harness during my degree. 
+               I have now found my way back to the design world with a career pivot to front-end web dev. I started my coding journey in the latter part of 2020 and haven't looked back!
+             </p>
+        </div>
+    </Container>
+    
+);
+
+export default AboutMe;
